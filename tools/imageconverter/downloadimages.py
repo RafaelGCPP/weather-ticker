98a76f59dd91e60934b456d815ce992440273368
Rafael Gustavo da Cunha Pereira Pinto <rafaelgcpp@gmail.com>
2026-01-02 17:03:44 -0300

    Add image download and conversion script for weather icons

--- tools/imageconverter/downloadimages.py
+++ tools/imageconverter/downloadimages.py
@@ -7,24 +7,14 @@ FILES=[
     {"source":"clear-night.svg", "destination":"01n.svg"}, # clear sky night
     {"source":"partly-cloudy-day.svg", "destination":"02d.svg"}, # few clouds day
     {"source":"partly-cloudy-night.svg", "destination":"02n.svg"}, # few clouds night
-    # {"source":"cloudy.svg", "destination":"03d.svg"}, # scattered clouds day
-    # {"source":"cloudy.svg", "destination":"03n.svg"}, # scattered clouds night
     {"source":"cloudy.svg", "destination":"03.svg"}, # scattered clouds night
-    # {"source":"overcast.svg", "destination":"04d.svg"}, # overcast clouds day
-    # {"source":"overcast.svg", "destination":"04n.svg"}, # overcast clouds night
     {"source":"overcast.svg", "destination":"04.svg"}, # overcast clouds night
-    # {"source":"rain.svg", "destination":"09d.svg"}, # shower rain day
-    # {"source":"rain.svg", "destination":"09n.svg"}, # shower rain night
     {"source":"rain.svg", "destination":"09.svg"}, # shower rain night
     {"source":"partly-cloudy-day-rain.svg", "destination":"10d.svg"}, # rain day
     {"source":"partly-cloudy-night-rain.svg", "destination":"10n.svg"}, # rain night
     {"source":"thunderstorms-day.svg", "destination":"11d.svg"}, # thunderstorm day
     {"source":"thunderstorms-night.svg", "destination":"11n.svg"}, # thunderstorm night
-    # {"source":"snow.svg", "destination":"13d.svg"}, # snow day
-    # {"source":"snow.svg", "destination":"13n.svg"}, # snow night
     {"source":"snow.svg", "destination":"13.svg"}, # snow night
-    # {"source":"mist.svg", "destination":"50d.svg"}, # mist day
-    # {"source":"mist.svg", "destination":"50n.svg"}, # mist night
     {"source":"mist.svg", "destination":"50.svg"}, # mist night
 ]
 
